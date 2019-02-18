7bba180278d3f930ae68c84cc84b0ef1f4517202
Mateus Santos <mateus.santos@dextra-sw.com>
2019-02-18 16:20:15 -0300

    feat(controller): created return message for post on index

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -8,4 +8,18 @@ exports.get = (ctx) => {
 
 exports.post = (ctx) => {
     userModel.create(ctx.request.body)
+    ctx.body = {
+        message: 'User created'
+    }
+}
+
+exports.userValidate = async (body) => {
+    const result = await userModel.findAll({
+        where: {
+            username: body.username
+        }
+    })
+
+    if (result.length > 0) return true
+    else return false
 }
