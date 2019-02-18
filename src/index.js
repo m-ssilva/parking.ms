98fa19141d1474b736e168654b5c617a8eebf879
Mateus Santos <mateus.santos@dextra-sw.com>
2019-02-18 09:22:21 -0300

    feat(index): adding registerRouter to index

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
 const path = require('path')
-const registerRoutes = require('')
+const registerRoutes = require('./helpers/register-routes')
 const bodyParser = require('koa-bodyparser')
 const logger = require('koa-logger')
+
+registerRoutes(router, path.join(__dirname, './routes'))
 
 app.use(bodyParser())
 app.use(logger())
