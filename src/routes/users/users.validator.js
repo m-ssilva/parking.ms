a734b7fdf46328297de7b11b28a845b29dd86c61
m-ssilva <bleed-it-out@live.com>
2019-03-18 19:59:44 -0300

    fix(validator): syntax changes

--- src/routes/users/users.validator.js
+++ src/routes/users/users.validator.js
@@ -5,7 +5,7 @@ const validatePost = async (body) => {
 
     let userValidator = await controller.userValidate(body)
 
-    if (!body.name) { errors.push({ messagE: 'Please enter a name.', path: 'body.name' }) }
+    if (!body.name) { errors.push({ message: 'Please enter a name.', path: 'body.name' }) }
     if (!body.username) { errors.push({ message: 'Please enter a username.', path: 'body.username' }) }
     if (!body.password) { errors.push({ message: 'Please enter a password.', path: 'body.password' }) }
     if (userValidator) { errors.push({ message: 'Username already in use.', path: 'body.username' }) }
